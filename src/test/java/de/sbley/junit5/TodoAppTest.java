69014c174b855f5561c76752f36828bf5ab5cbf5
Stefan Bley <stefan.bley@saxsys.de>
2016-11-04 16:52:42 +0100

    todoapp.remove (assertions finished)

--- src/test/java/de/sbley/junit5/TodoAppTest.java
+++ src/test/java/de/sbley/junit5/TodoAppTest.java
@@ -2,6 +2,7 @@ package de.sbley.junit5;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.expectThrows;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -17,17 +18,44 @@ public class TodoAppTest {
     @Test
     public void addTodo() {
         Todo todo = new Todo("Learn JUnit5", LocalDate.of(2016, Month.NOVEMBER, 14));
-
         TodoApp todoApp = new TodoApp();
+
+        // test
         todoApp.addTodo(todo);
 
+        // assert
         List<Todo> todos = todoApp.getTodoList();
-        assertEquals(1, todos.size(), "todo list should contain 1 element");
+        assertEquals(1, todos.size(), () -> "todo list should contain 1 element");
 
         assertAll("todo item should contain correct values", () -> {
             assertEquals("Learn JUnit5", todos.get(0).getTitle());
         }, () -> {
             assertEquals(LocalDate.of(2016, Month.NOVEMBER, 14), todos.get(0).getDueDate());
         });
+    }
+
+    @Test
+    public void removeTodo() {
+        Todo todo = new Todo("Learn JUnit5", LocalDate.of(2016, Month.NOVEMBER, 14));
+        TodoApp todoApp = new TodoApp();
+        todoApp.addTodo(todo);
+
+        // test
+        todoApp.remove(todo);
+
+        // assert
+        assertEquals(0, todoApp.getTodoList().size());
+    }
+
+    @Test
+    public void removeTodo_throwExceptionIfTodoIsNotInList() {
+        Todo todo = new Todo("Learn JUnit5", LocalDate.of(2016, Month.NOVEMBER, 14));
+        TodoApp todoApp = new TodoApp();
+
+        // test & assert
+        IllegalArgumentException exception = expectThrows(IllegalArgumentException.class, () -> {
+            todoApp.remove(todo);
+        });
+        assertEquals("Invalid todo", exception.getMessage());
     }
 }
